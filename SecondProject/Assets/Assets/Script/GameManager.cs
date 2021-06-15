98ba6d290eae0613a8ca06272095a02086f2080d
choiyoungwoo <duddn4646@gmail.com>
2021-06-15 17:49:37 +0900

    PlayerPrefabs

--- SecondProject/Assets/Assets/Script/GameManager.cs
+++ SecondProject/Assets/Assets/Script/GameManager.cs
@@ -17,16 +17,93 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+
+        if (!isStarted)
+        {
+            SetUnitStatus("Unit1", 2, 1, 1, 1, 1, 0);
+            SetUnitStatus("Unit2", 1, 10, 10, 10, 10, 0);
+            SetUnitStatus("Unit3", 0, 20, 20, 20, 20, 0);
+            SetUnitStatus("Unit4", 0, 30, 30, 30, 30, 0);
+            SetUnitStatus("Unit5", 0, 30, 30, 30, 30, 0);
+            SetUnitStatus("Unit6", 0, 30, 30, 30, 30, 0);
+            SetUnitStatus("Unit7", 0, 30, 30, 30, 30, 0);
+            SetUnitStatus("Unit8", 0, 30, 30, 30, 30, 0);
+            SetUnitStatus("Unit9", 0, 30, 30, 30, 30, 0);
+
+            PlayerPrefs.SetInt("MyMoney", 1000);
+
+
+        }
+
+
+
+
     }
     #endregion
+
+    bool isStarted = false;
+
+
+    public void SetUnitStatus(string UnitName,int Lock ,int HP, int ATK, int SPEED, int DELAY, int Lv)
+    {
+        //게임을 첫 번째로 실행했다면 기본 정보 만들기
+        //임시로 유닛3까지만 만듬
+        int[] number = new int[6];
+
+        number[0] = Lock;
+        number[1] = HP; 
+        number[2] = ATK;
+        number[3] = SPEED;
+        number[4] = DELAY;
+        number[5] = Lv;
+
+        string strArr = "";
+
+        //배열과 , 를 번갈아가면서 저장
+        for (int i = 0; i < number.Length; i++)
+        {
+            strArr = strArr + number[i];
+
+            if (i < number.Length - 1) // 마지막에는 ,를 넣지 않음
+            {
+                strArr = strArr + ",";
+            }
+        }
+
+        PlayerPrefs.SetString(UnitName, strArr);
+    }
+
+    public void SetUnitStatus(string UnitName, int[] UnitStatus)
+    {
+        //게임을 첫 번째로 실행했다면 기본 정보 만들기
+        //임시로 유닛3까지만 만듬
+
+        string strArr = "";
+
+        //배열과 , 를 번갈아가면서 저장
+        for (int i = 0; i < UnitStatus.Length; i++)
+        {
+            strArr = strArr + UnitStatus[i];
+
+            if (i < UnitStatus.Length - 1) // 마지막에는 ,를 넣지 않음
+            {
+                strArr = strArr + ",";
+            }
+        }
+
+        PlayerPrefs.SetString(UnitName, strArr);
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
+        
+
         //유닛들의 기본 정보 가져와서 뿌려주기
         /*
-
+            
 
 
 
--- SecondProject/Assets/Assets/Script/SelectUnit.cs
+++ SecondProject/Assets/Assets/Script/SelectUnit.cs
@@ -8,12 +8,43 @@ public class SelectUnit : MonoBehaviour
 
     Button btn;
     string UnitName;
+    int[] UnitStatus;
+    public GameObject NextUnit;
 
+
+
+    public void UnLock()
+    {
+        LoadData();
+
+        UnitStatus[0] = 1;
+        GameManager.instance.SetUnitStatus(UnitName, UnitStatus);
+
+        //이미지 바꿔주기
+        SetImage();
+        GetComponent<Image>().raycastTarget = true;
+
+
+        
+    }
+
+    public void LoadData()
+    {
+        string[] dataArr = PlayerPrefs.GetString(UnitName).Split(',');
+
+        UnitStatus = new int[dataArr.Length];
+
+        for (int i = 0; i < dataArr.Length; i++)
+        {
+            UnitStatus[i] = System.Convert.ToInt32(dataArr[i]);
+        }
+    }
 
 
     public void OnClickButton()
     {
-        UnitName = name;
+        LoadData();
+
 
 
         //UnitName을 이용하여 유닛 설명판 띄우기
@@ -24,14 +55,44 @@ public class SelectUnit : MonoBehaviour
 
         //해금 상태에 따라
         //0 : 해금되어 있지 않은 유닛은 터치 불가능!!
-        //1 : 해금 할 수 있으면 해금하기
-        //2 : 해금이 되어 있다면 렙 올리기
-        
+        if (UnitStatus[0] ==0 )
+        {
+            //빨간 이미지 상태
+
+        }
+        //1 : 구매 할 수 있으면 구매하기
+        else if (UnitStatus[0] == 1)
+        {
+            //회색 이미지 상태
+            //내가 가진 돈이 요구치 보다 많다면 해금 
+            if(PlayerPrefs.GetInt("MyMoney") > 100 )
+            {
+
+                transform.Find("Text").GetComponent<Text>().text = ++UnitStatus[5] + "/20";
+                Debug.Log("해금됨");
+
+                //유닛스텟 업데이트 하기
+                UnitStatus[0] = 2;
+
+                //스프라이트 바꾸기
+                //transform.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+                //transform.Find("Unit_img").GetComponent<Image>().color = new Color(255, 255, 255, 255);
+                
+                //다음 유닛 언락하기
+                if(NextUnit != null)
+                    NextUnit.GetComponent<SelectUnit>().UnLock();
 
 
+                GameManager.instance.SetUnitStatus(UnitName,UnitStatus);
+            }
 
+        }
+        //2 : 구매 되어 있다면 렙 올리기
+        else if (UnitStatus[0] == 2)
+        {
+            //업글 가능 상태
 
-
+        }
 
         Debug.Log(UnitName);
     }
@@ -41,5 +102,35 @@ public class SelectUnit : MonoBehaviour
     {
         btn = GetComponent<Button>();
         btn.onClick.AddListener(OnClickButton);
+
+        UnitName = name;
+
+        SetImage();
+
     }
+
+    void SetImage()
+    {
+        LoadData();
+
+        //UnLock
+        if(UnitStatus[0] == 1)
+        {
+            GetComponent<Image>().color = new Color(100, 100, 100, 255);
+            transform.Find("Unit_img").GetComponent<Image>().color = new Color(100, 100, 100, 255);
+        }
+        /*
+        else if(UnitStatus[0] == 2)
+        {
+            GetComponent<Image>().color = new Color(255, 255, 255, 255);
+            transform.Find("Unit_img").GetComponent<Image>().color = new Color(255, 255, 255, 255);
+        }
+
+         */
+
+    }
+
+
+
+
 }
